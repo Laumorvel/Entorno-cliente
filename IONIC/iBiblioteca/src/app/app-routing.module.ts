d9a73db660473b1e936c54fda3f46854510ce7cc
lauramolez <lauramorelez@gmail.com>
2022-01-26 17:05:26 +0100

    ibiblioteca

--- IONIC/iBiblioteca/src/app/app-routing.module.ts
+++ IONIC/iBiblioteca/src/app/app-routing.module.ts
@@ -7,10 +7,22 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'biblioteca',
+    loadChildren: () => import('./biblioteca/biblioteca.module').then( m => m.BibliotecaPageModule)
+  },
+  {
+    path: 'curriculum',
+    loadChildren: () => import('./curriculum/curriculum.module').then( m => m.CurriculumPageModule)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'autor',
+    loadChildren: () => import('./autor/autor.module').then( m => m.AutorPageModule)
+  }
 ];
 
 @NgModule({
--- IONIC/iBiblioteca/src/app/app.module.ts
+++ IONIC/iBiblioteca/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  exports: [],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
